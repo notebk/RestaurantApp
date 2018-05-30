d0395b870ede5715ada65b3495b88f222cba7196
notebk <egzz93@me.com>
2018-05-30 07:20:25 -0400

    Boilerplate

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-console.log('hello world');
-console.log('hi back');
+var exphbs = require('express-handlebars');
+var express = require('express');
+var index = require('./routes/index');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//setting default layout of app to main.handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//sets body parsing, cookie parsing, and makes public directory a static folder
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.home);
+
+app.listen(3000);
