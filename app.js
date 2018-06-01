c33cd084832007125d7dad4d53ad6e791c893921
notebk <egzz93@me.com>
2018-06-01 07:50:01 -0400

    Removed unnecessary code previous app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//database connection
 mongoose.connect('mongodb://localhost/robots');
 
 var app = express();
@@ -23,9 +24,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', index.home);
-app.get('/cats/new', cats.new);
-app.get('/cats', cats.list);
-app.get('/cats/bycolor/:color', cats.byColor);
-app.get('/cats/delete/old', cats.delete);
 
 app.listen(3000);
